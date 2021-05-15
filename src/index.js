a3664fc5b84ed718a6820ebcce4570c21b5fd668
imseongtae <dev.haemil@gmail.com>
2021-05-16 06:37:30 +0900

    feat: Create news page using airtable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-require('./style/reset.css');
-require('./style/style.css');
+require('./style/_reset.css');
+require('./style/_style.css');
 
-var div = document.querySelector('.container');
+// control loading spinner
+const spinnerWrapper = document.querySelector('.loading-spinner-wrapper');
+spinnerWrapper.style.display = 'block';
 
-div.innerText = 'Wepback loaded!!';
+const iframe = document.createElement('iframe');
+iframe.classList.add('airtable');
+iframe.style.display = 'none';
+iframe.src =
+  'https://airtable.com/embed/shrgZSDljNoy4iDzt?backgroundColor=cyanLight&viewControls=on';
+
+// this function will called when the iframe loaded
+iframe.onload = function () {
+  spinnerWrapper.style.display = 'none';
+  iframe.style.display = 'block';
+};
+// set the src last.
+// iframe.src =
+//   'https://airtable.com/embed/shrgZSDljNoy4iDzt?backgroundColor=cyanLight&viewControls=on';
+
+// add it to the page.
+const airtableWrapper = document.querySelector('.airtable-wrapper');
+airtableWrapper.appendChild(iframe);
