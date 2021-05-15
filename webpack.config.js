5195c7c04c0e0a43cbe546fa72fc153f8120fb56
imseongtae <dev.haemil@gmail.com>
2021-05-15 20:02:28 +0900

    chore: webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 9000,
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+      // template: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+};
