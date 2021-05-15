082b5b3df5dc50e6167db215b3b55f324e3d0555
imseongtae <dev.haemil@gmail.com>
2021-05-16 02:29:19 +0900

    chore: stylelint setting

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'selector-list-comma-newline-after': null,
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   mode: 'none',
@@ -25,6 +26,14 @@ module.exports = {
       // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
       // template: 'index.html',
       template: 'src/index.html',
+    }),
+    new StyleLintPlugin({
+      configFile: '.stylelintrc.js',
+      context: 'src',
+      files: '**/*.css',
+      failOnError: false,
+      quiet: false,
+      emitErrors: true, // by default this is to true to check the CSS lint errors
     }),
   ],
 };
